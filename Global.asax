920431f9f6fbda2afc3f63b29d2e7e019eef3e56
Daniel Gustafsson <shoffaloffa@gmail.com>
2016-09-20 14:35:38 +0200

    My new shoffa Grid.
    SASS proj for grid

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sass.grid.web.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sass.grid.web.Startup))]
+namespace sass.grid.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
